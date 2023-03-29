f32c37489e2757fa273aa591877bbdf084f70b51
eliasfernandescout <eliasfernandescout@gmail.com>
2023-03-29 15:27:57 -0300

    update

---
+++ src/NumeroAleatorio.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class NumeroAleatorio {
+
+    public static void main(String[] args){
+        Random numeroAleatorio = new Random();
+
+        System.out.println(numeroAleatorio.nextInt(6));
+
+
+
+    }
+}
