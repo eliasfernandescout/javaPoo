38ef7e15fc90d04830fd890ef2f89bdd357ad2e4
eliasfernandescout <eliasfernandescout@gmail.com>
2023-03-30 14:08:11 -0300

    upadte

---
+++ src/Contants.java
@@ -0,0 +1,10 @@
+public class Contants {
+    public static void main(String[] args){
+        final int NUMERO_MAXIMO = 10;
+        System.out.println(NUMERO_MAXIMO);
+
+        for(int i = 0; i < NUMERO_MAXIMO; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/Vetores.java
@@ -0,0 +1,9 @@
+public class Vetores {
+    public static void main(String[] args){
+        int[] vetor = {1,2,3,4};
+        System.out.println(vetor[0]);
+        String[] vetorString = {"ELIAS", "DANTE", "JHESS"};
+        System.out.println(vetorString[0]);
+
+    }
+}
