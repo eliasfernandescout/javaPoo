c4749aa025e12017e981cf1c478d1d90fb2bfc26
eliasfernandescout <eliasfernandescout@gmail.com>
2023-04-16 22:52:40 -0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-        Scanner keyboardInput = new Scanner(System.in);
-
-        System.out.println("Quantos anos voce tem: ");
-        int idade = 0;
-        idade = keyboardInput.nextInt();
-        keyboardInput.nextLine();
-
-        System.out.println("Qual e o seu sexo: ");
-        String sexo = keyboardInput.nextLine();
-
-        System.out.println("Qual e a sua altura: ");
-        double altura = keyboardInput.nextDouble();
-        keyboardInput.nextLine();
-
-        System.out.println("Idade lida:" + idade);
-        System.out.println("Altura lida:" + altura);
-        System.out.println("Sexo lido:" + sexo);
-        System.out.println(idade + sexo + altura);
-
+        System.out.println("Hello World");
     }
 }
