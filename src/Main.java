4ef64024ff3049098d02d5b7c2216027d28b710e
eliasfernandescout <eliasfernandescout@gmail.com>
2023-04-20 12:45:31 -0300

    poo update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Worldk");
+        Carro meuCarro = new Carro();
+
+        meuCarro.anoFabricacao = 2022;
+        meuCarro.cor = "Cinza";
+        meuCarro.fabricante = "Toyota";
+        meuCarro.modelo = "Toyota Corolla Cross";
+
+        System.out.println(meuCarro.anoFabricacao);
+        System.out.println(meuCarro.modelo);
+
+
+
+
     }
 }
