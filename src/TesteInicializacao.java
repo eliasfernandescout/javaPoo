8d57a399be5ea9e460f938f7756e50ac98703417
eliasfernandescout <eliasfernandescout@gmail.com>
2023-04-25 19:14:13 -0300

    poo update

--- src/TesteInicializacao.java
+++ src/TesteInicializacao.java
@@ -5,6 +5,7 @@ public class TesteInicializacao {
         meuCarro.fabricante = "Honda";
         meuCarro.anoFabricacao = 2021;
 
+
         System.out.println(meuCarro.fabricante);
         System.out.println(meuCarro.modelo);
         System.out.println(meuCarro.anoFabricacao);
