530c408b9ce6bf8930e3d6c6bb99acad6dcedb2e
eliasfernandescout <eliasfernandescout@gmail.com>
2023-06-17 20:57:02 -0300

    poo update

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,39 @@
 public class Main {
     public static void main(String[] args) {
 
-        Pessoa pessoa1 = new Pessoa();
-        pessoa1.nome = "Elias Fernandes";
-        pessoa1.cpf = "147.314.467-16";
-        pessoa1.anoNascimento = 1991;
+//        Pessoa pessoa1 = new Pessoa();
+//        pessoa1.nome = "Elias Fernandes";
+//        pessoa1.cpf = "147.314.467-16";
+//        pessoa1.anoNascimento = 1991;
+//
+//        Carro meuCarro = new Carro();
+//        meuCarro.anoFabricacao = 2022;
+//        meuCarro.cor = "Cinza";
+//        meuCarro.fabricante = "Toyota";
+//        meuCarro.modelo = "Toyota Corolla Cross";
+//        meuCarro.proprietario = pessoa1;
+////        meuCarro.proprietario = new Pessoa();
+////        meuCarro.proprietario.nome = "Elias Fernandes";
+////        meuCarro.proprietario.cpf ="147.314.467-16";
+//
+//        Carro seuCarro = new Carro();
+//        seuCarro.anoFabricacao = 2014;
+//        seuCarro.cor = "Branco";
+//        seuCarro.fabricante = "Fiat";
+//        seuCarro.modelo = "Punto Sporting";
+//        seuCarro.proprietario = new Pessoa();
+//        seuCarro.proprietario.nome = "Jhessiny Mattos";
+//        seuCarro.proprietario.cpf="155.798.387-94";
+//
+//        System.out.println(meuCarro.anoFabricacao + " - " + meuCarro.modelo + " - " + meuCarro.proprietario.nome);
+//
+//
+//        System.out.println(seuCarro.anoFabricacao + " - " + seuCarro.modelo + " - " + seuCarro.proprietario);
 
-        Carro meuCarro = new Carro();
-        meuCarro.anoFabricacao = 2022;
-        meuCarro.cor = "Cinza";
-        meuCarro.fabricante = "Toyota";
-        meuCarro.modelo = "Toyota Corolla Cross";
-        meuCarro.proprietario = pessoa1;
-//        meuCarro.proprietario = new Pessoa();
-//        meuCarro.proprietario.nome = "Elias Fernandes";
-//        meuCarro.proprietario.cpf ="147.314.467-16";
+        Pessoa pessoa2 = new Pessoa();
+        pessoa2.nome = "Dante Mattos";
 
-        Carro seuCarro = new Carro();
-        seuCarro.anoFabricacao = 2014;
-        seuCarro.cor = "Branco";
-        seuCarro.fabricante = "Fiat";
-        seuCarro.modelo = "Punto Sporting";
-        seuCarro.proprietario = new Pessoa();
-        seuCarro.proprietario.nome = "Jhessiny Mattos";
-        seuCarro.proprietario.cpf="155.798.387-94";
-
-        System.out.println(meuCarro.anoFabricacao + " - " + meuCarro.modelo + " - " + meuCarro.proprietario.nome);
-
-
-        System.out.println(seuCarro.anoFabricacao + " - " + seuCarro.modelo + " - " + seuCarro.proprietario);
-
+        System.out.println(pessoa2.nome);
 
 
 
