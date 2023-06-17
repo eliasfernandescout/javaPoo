7b7fcc1a56c111594266ce0ced2733ca6120f15a
eliasfernandescout <eliasfernandescout@gmail.com>
2023-06-17 17:12:13 -0300

    poo update

--- src/TesteReferencias.java
+++ src/TesteReferencias.java
@@ -4,7 +4,7 @@ public class TesteReferencias {
         Pessoa pessoa1 = new Pessoa();
         pessoa1.nome = "Dante";
 
-        Carro meuCarro = new Carro();
+        Carro meuCarro = new Carro(); // A variavel nao armazena, mas sim faz referencia ao objeto
         meuCarro.modelo = "HR-V";
         meuCarro.proprietario = pessoa1;
 
