0eb59e14a94c493bd336ef6f63b991f37e73f59b
eliasfernandescout <eliasfernandescout@gmail.com>
2023-06-30 23:40:59 -0300

    poo update

--- src/api/consultorio/MainConsultorio.java
+++ src/api/consultorio/MainConsultorio.java
@@ -3,11 +3,15 @@ package api.consultorio;
 public class MainConsultorio {
     public static void main(String[] args) {
         CalculadoraImc calculadoraImc = new CalculadoraImc();
-        double peso = 134;
-        double altura = 1.91;
+
+        Paciente pacienteElias = new Paciente(134, 1.91);
+//        double peso = 134;
+//        double altura = 1.91;
+        pacienteElias.peso = 134;
+        pacienteElias.altura = 1.95;
         String nome = "Elias Fernandes";
 
-        IndiceMassaCorporal imc = calculadoraImc.calcular(peso, altura);
+        IndiceMassaCorporal imc = calculadoraImc.calcular(pacienteElias);
 //        System.out.println((imc.estaComObesidade()? "SIM" : "NAO")); OPERADOR TERNARIO
         if(imc.estaComObesidade()){
             System.out.printf("Paciente com altura %.2f e peso de %.2f " + "esta com obesidade\n", imc.altura, imc.peso);
---
+++ src/api/consultorio/Paciente.java
@@ -0,0 +1,13 @@
+package api.consultorio;
+
+public class Paciente {
+    double altura;
+    double peso;
+
+    public Paciente(double peso, double altura) {
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+
+}
