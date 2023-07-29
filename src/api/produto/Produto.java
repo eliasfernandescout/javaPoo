181efbd5d027e6d1bc02952ccc2a6aada8ca773e
eliasfernandescout <eliasfernandescout@gmail.com>
2023-07-29 19:24:45 -0300

    this

--- src/api/produto/Produto.java
+++ src/api/produto/Produto.java
@@ -3,4 +3,9 @@ package api.produto;
 public class Produto {
     double precoCusto;
     double precoVenda;
+
+    void alterarPrecoCusto(double precoCusto){
+        this.precoCusto = precoCusto;
+
+    }
 }
